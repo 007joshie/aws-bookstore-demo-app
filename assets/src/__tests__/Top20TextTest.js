7d01b0dcbb1c5e1bafb879c199221277830178b3
BOAG, JOSHUA CLAYTON (Josh) <joshua.boag@gmail.com>
2021-05-30 16:19:48 +1200

    'Added

--- assets/src/__tests__/Top20TextTest.js
+++ assets/src/__tests__/Top20TextTest.js
@@ -11,9 +11,19 @@ configure({ adapter: new Adapter() });
 
 import BestSellers from '../modules/bestSellers/BestSellers.tsx';
 
+import PastPurchases from '../modules/pastPurchases/PastPurchases.tsx';
+
   it('Render check Text \'Top 20 best sellers\'', function() {
     window.alert = jest.fn(); // Surpress Bug alert from https://stackoverflow.com/questions/55787988/window-alert-not-implemented-when-running-tests
     expect(shallow(<BestSellers />).contains('Top 20 best sellers')).toBe(true);
+  });
+
+  it('Render check Text \'Past Purchases\'', function() {
+    window.alert = jest.fn(); // Surpress Bug alert from https://stackoverflow.com/questions/55787988/window-alert-not-implemented-when-running-tests
+    expect(shallow(<PastPurchases />).contains(
+    <div className="white-box">
+      <h3>Past purchases</h3>
+    </div>)).toBe(true);
   });
   
   
